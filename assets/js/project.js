60121f835f504c3c3cb5a47d510cc0d64fe41d95
Alejandro Shadow <121072134+derkila@users.noreply.github.com>
2023-01-01 19:07:34 -0600

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -6,12 +6,12 @@ const projectcards = document.querySelector(".projectcards");
 // Array of object for projects
 const projects = [
   {
-    title: "ytDownloader",
-    cardImage: "assets/images/ytDownloader-banner.png",
-    description: " A YouTube Video Downloader Made to Download High Quality YouTube Videos with a Simple GUI.",
-    tagimg: "https://github.com/itzkavindu/itzkavindu.github.io/blob/master/assets/images/ytDownloader-banner.png",
-    Previewlink: "https://github.com/itzkavindu/ytDownloader",
-    Githublink: "https://github.com/itzkavindu/ytDownloader",
+    title: "AnimeDEX",
+    cardImage: "assets/amdex/home.png",
+    description: " Watch anime for free in the best quality",
+    tagimg: "assets/amdex/home.png",
+    Previewlink: "https://animedex.tk",
+    Githublink: "https://github.com/derkila,
   },
 
   {
